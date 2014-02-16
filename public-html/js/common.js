248e8420dff04ae3b597d4116d1b7f0db1a09656
Luca De Franceschi <luca.defranceschi.91@gmail.com>
2014-02-16 16:50:49 +0100

    Creato javascript per la navigazione (menu_item)

--- public-html/js/common.js
+++ public-html/js/common.js
@@ -2,10 +2,31 @@ $(document).ready(
 	
 	function() {
 	
+	/* Imposto le due colonne con altezza uguale */
+
 	var height = Math.max($("#header").height(), $("#content").height());
 	$("#header").height(height);
 	$("#content").height(height);
- 
+
+	/* Imposto il comportamento per le voci di menu */
+
+	$(".menu_item").mouseover(function() {
+		$(this).children("a").css("color", "red");
+		$(this).css("border-color", "#FFF");
+	});
+
+	$(".menu_item").mouseout(function() {
+		$(this).children("a").css("color", "#FFF");
+		$(this).css("border-color", "#555");
+	});
+
+	$(".menu_item").click(function() {
+		url = $(this).children("a").attr("href");
+		window.location = url;
+	});
+	
+	/* Imposto la slideshow di sfondo */
+
  	i = 1;
 	setInterval(function(){
 		$('body').css({backgroundImage : 'url(' + images[i] + ')'});
@@ -13,5 +34,8 @@ $(document).ready(
 		if (i == images.length) i = 0;
 	}, 5000);
 });
+
+
+/* Array di immagini da visualizzare nella slideshow di sfondo */
 
 var images = ['images/slideshow/1.jpg', 'images/slideshow/2.jpg', 'images/slideshow/3.jpg', 'images/slideshow/4.jpg', 'images/slideshow/5.jpg'];
