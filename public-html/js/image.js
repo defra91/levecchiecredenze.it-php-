79f5b2e8f86820fb6c5588598d00174d47e95bd8
Dquaglio <skyzzo92@gmail.com>
2014-03-22 20:24:07 +0100

    insert image

--- public-html/js/image.js
+++ public-html/js/image.js
@@ -2,6 +2,21 @@ $(document).ready(function () {
    $("#filePhoto").change(function() {
            readURL(this);
        });
+   $("#form").submit(function(){
+  var isFormValid = true;
+    $("#form input:text").each(function(){ // Note the :text
+        if ($.trim($(this).val()).length == 0){
+            $(this).addClass("highlight");
+            isFormValid = false;
+        } else {
+            $(this).removeClass("highlight");
+        }
+    });
+    if (!isFormValid)
+      event.preventDefault();
+    return isFormValid;
+  
+  });
 });
 var readURL=function(input) {
            if (input.files && input.files[0]) {
