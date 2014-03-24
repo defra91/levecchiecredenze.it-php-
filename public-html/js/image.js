f634cee182f03e7435106daee0684d765dbec666
Dquaglio <skyzzo92@gmail.com>
2014-03-24 10:27:50 +0100

    aa

--- public-html/js/image.js
+++ public-html/js/image.js
@@ -23,7 +23,7 @@ var readURL=function(input) {
                var reader = new FileReader();
                reader.onload = function(e) {
                     var immagine=$("<img src='' id='previewHolder' alt='anteprima immagine da inserire'/>");
-                    $('#imgine').append(immagine);
+                    $('#filePhoto').append(immagine);
                    $('#previewHolder').attr('src', e.target.result);
                }
 
