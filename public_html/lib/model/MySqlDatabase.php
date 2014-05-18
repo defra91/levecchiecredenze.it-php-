c835e26c2800333bbdf388b60cf5a28bb70e7a34
Luca De Franceschi <luca.defranceschi.91@gmail.com>
2014-05-18 16:18:18 +0200

    Inserita classe per gestione dei Log

--- public_html/lib/model/MySqlDatabase.php
+++ public_html/lib/model/MySqlDatabase.php
@@ -1,32 +1,11 @@
 <?php
 include_once("../controller/Error.php");
 /**
-* This class is responsible to provide operations for mysql database
+* This class provides operations for mysql database
 */
-class Database
+class MySqlDatabase
 {
 	/**
-	* Specifies a host name or an IP address
-	*/
-	private $dbHost;
-	/**
-	* Specifies the MySQL username
-	*/
-	private $dbUser;
-	/**
-	* Specifies the MySQL password
-	*/
-	private $dbPassword;
-	/**
-	* Specifies the default database to be used
-	*/
-	private $dbName;
-	/*
-	* Specifies the port number to attempt to connect to the MySQL server
-	*/
-	private $dbPort;
-
-	/*
 	* This is the public constructor of the class
 	*/
 	public function __construct($dbHost, $dbUser, $dbPassword, $dbName, $dbPort) {
@@ -37,35 +16,35 @@ class Database
 		$this->dbPort = $dbPort;
 	}
 
-	/*
+	/**
 	* Returns the value of the data field dbHost
 	*/
 	public function getDbHost() {
 		return $this->dbHost;
 	}
 
-	/*
+	/**
 	* Returns the value of the data field dbUser
 	*/
 	public function getDbUser() {
 		return $this->dbUser;
 	}
 
-	/*
+	/**
 	* Returns the value of the data field dbPassword
 	*/
 	public function getDbPassword() {
 		return $this->dbPassword;
 	}
 
-	/*
+	/**
 	* Returns the value of the data field dbName
 	*/
 	public function getDbName() {
 		return $this->dbName;
 	}
 
-	/*
+	/**
 	* Connect to the database
 	*/
 	public function dbConnect() {
@@ -78,7 +57,6 @@ class Database
 			return $connection;
 		}
 	}
-
 }
 
 ?>
